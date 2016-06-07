fcbdcecef7adb0a3a4044a504df2b76c43bf7d7a
Diego Sampaio <chinello@gmail.com>
2016-06-07 09:21:39 -0300

    Code formatting

--- gulpfile.js
+++ gulpfile.js
@@ -7,60 +7,61 @@ var plugins = require('gulp-load-plugins')();
 
 // Function to clean files using del()
 function clean(path) {
-  return del(path);
+	return del(path);
 }
 
 // Object with directory paths for further usage
 var dirs = {
-  source: './src',
-  build: './build'
+	source: './src',
+	build: './build'
 };
 
 // Task to compile using metalsmith
-gulp.task('metalsmith', function (cb) {
-  exec('node index.js', function (err) {
-    if (!err) {
-      gulp.src(dirs.build+'/**/*')
-        .pipe(plugins.connect.reload());
-    }
-    cb(err);
-  });
+gulp.task('metalsmith', function(cb) {
+	exec('node index.js', function(err) {
+		if (!err) {
+			gulp.src(dirs.build + '/**/*')
+				.pipe(plugins.connect.reload());
+		}
+		cb(err);
+	});
 })
 
 // Connect task to serve web server and reload automatically
 gulp.task('connect', function() {
-  plugins.connect.server({
-    root: 'build',
-    livereload: true,
-    port: 8090
-  });
+	plugins.connect.server({
+		root: 'build',
+		livereload: true,
+		port: 8090
+	});
 });
 
 // Task to clean/trash folders
-gulp.task('clean', function () {
-  var files = [
-    dirs.source,
-    dirs.build
-  ];
+gulp.task('clean', function() {
+	var files = [
+		dirs.source,
+		dirs.build
+	];
 
-  return clean(files);
+	return clean(files);
 });
 
 // Watch for changes in files
 gulp.task('watch', function() {
-  gulp.watch(dirs.source + '/**/*', ['metalsmith']);
-  gulp.watch('./assets/**/*', ['metalsmith']);
-  gulp.watch('./templates/**/*', ['metalsmith']);
- });
+	gulp.watch(dirs.source + '/**/*', ['metalsmith']);
+	gulp.watch('./assets/**/*', ['metalsmith']);
+	gulp.watch('./templates/**/*', ['metalsmith']);
+});
 
 // Git clone task to fetch source files from Rocket.Chat.Docs which is also
 // dependent on the 'clean' task
-gulp.task('git', ['clean'], function(){
-  plugins.git.clone('https://github.com/RocketChat/Rocket.Chat.Docs', { args: dirs.source }, function (err) {
-    if (err) throw err;
-  });
+gulp.task('git', ['clean'], function() {
+	plugins.git.clone('https://github.com/RocketChat/Rocket.Chat.Docs', { args: dirs.source }, function(err) {
+		if (err) throw err;
+	});
 });
 
 // Register tasks
 gulp.task('fetch', ['git']);
 gulp.task('default', ['connect', 'metalsmith', 'watch']);
+
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var Metalsmith  = require('metalsmith');
-var markdown    = require('metalsmith-markdown');
-var layouts     = require('metalsmith-layouts');
-var permalinks  = require('metalsmith-permalinks');
+var Metalsmith = require('metalsmith');
+var markdown = require('metalsmith-markdown');
+var layouts = require('metalsmith-layouts');
+var permalinks = require('metalsmith-permalinks');
 var metallic = require('metalsmith-metallic');
 var each = require('metalsmith-each');
 var assets = require('metalsmith-assets');
@@ -15,49 +15,50 @@ var slugifyPath = require('slugify-path').default;
 var headingsidentifier = require("metalsmith-headings-identifier");
 
 Metalsmith(__dirname)
-  .metadata({
-    title: "Rocket.Chat Docs",
-    description: "Rocket.Chat Docs",
-    generator: "Metalsmith",
-    url: "http://www.metalsmith.io/"
-  })
-  .source('./src')
-  .destination('./build')
-  .clean(true)
-  .use(ignore([
-    '.git/*',
-    '.git*'
-  ]))
-  .use(parseGitHubLinks())
-  .use(each(function (file, filename) {
-      return filename.replace(/README\.md/, 'index.md');
-    }
-  ))
-  // .use(slug())
-  // .use(permalinks({
-  //   pattern: ':title'
-  // }))
-  .use(markdown({
-    smartypants: true,
-    gfm: true,
-    tables: true
-  }))
-  .use(metallic())
-  .use(generateMenu())
-  .use(slugifyFiles())
-  .use(layouts({
-    engine: 'handlebars',
-    default: 'layout.html',
-    directory: 'templates',
-    partials: 'templates'
-  }))
-  .use(headingsidentifier({
-    linkTemplate: "<a class='myCustomHeadingsAnchorClass' href='#%s'><span></span></a>"
-  }))
-  .use(assets({
-    source: './assets', // relative to the working directory
-    destination: './assets' // relative to the build directory
-  }))
-  .build(function(err, files) {
-    if (err) { throw err; }
-  });
+	.metadata({
+		title: "Rocket.Chat Docs",
+		description: "Rocket.Chat Docs",
+		generator: "Metalsmith",
+		url: "http://www.metalsmith.io/"
+	})
+	.source('./src')
+	.destination('./build')
+	.clean(true)
+	.use(ignore([
+		'.git/*',
+		'.git*'
+	]))
+	.use(parseGitHubLinks())
+	.use(each(function(file, filename) {
+		return filename.replace(/README\.md/, 'index.md');
+	}))
+	// .use(slug())
+	// .use(permalinks({
+	//   pattern: ':title'
+	// }))
+	.use(markdown({
+		smartypants: true,
+		gfm: true,
+		tables: true
+	}))
+	.use(metallic())
+	.use(generateMenu())
+	.use(slugifyFiles())
+	.use(layouts({
+		engine: 'handlebars',
+		default: 'layout.html',
+		directory: 'templates',
+		partials: 'templates'
+	}))
+	.use(headingsidentifier({
+		linkTemplate: "<a class='myCustomHeadingsAnchorClass' href='#%s'><span></span></a>"
+	}))
+	.use(assets({
+		source: './assets', // relative to the working directory
+		destination: './assets' // relative to the build directory
+	}))
+	.build(function(err, files) {
+		if (err) {
+			throw err; }
+	});
+
