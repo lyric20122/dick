6648acdc57c90ec7f6b0547dac59ff7fd4288f06
Diego Sampaio <chinello@gmail.com>
2016-06-27 10:25:54 -0300

    Code cleanup

--- plugins/generate-menu.js
+++ plugins/generate-menu.js
@@ -63,9 +63,6 @@ function generateMenu() {
 			createTree(fileTree, fileParts);
 		});
 
-		var level = 1;
-		// var folders = [];
-
 		var folders = createMenu(fileTree, '', '', 1);
 
 		var newFolders = _.sortBy(folders, 'sortTitle');
@@ -77,79 +74,6 @@ function generateMenu() {
 		done();
 	}
 }
-
-function generateMenuOld() {
-	return function(files, metalsmith, done) {
-		var folders = [];
-		var unique = {};
-
-		for (var file in files) {
-			var data = files[file];
-			var slash = file.indexOf('/');
-			if (!slash) {
-				continue;
-			}
-			var oneLevel = file.substr(0, file.indexOf('/'));
-
-			if (oneLevel == '' || oneLevel == '.git') {
-				continue;
-			}
-
-			var secondLevel = file.substr(file.indexOf('/') + 1);
-			// console.log('secondLevel ->',secondLevel);
-			secondLevel = secondLevel.substr(0, secondLevel.indexOf('/'));
-
-			if (secondLevel != '') {
-
-				if (unique[oneLevel+'/'+secondLevel]) {
-					continue;
-				}
-				unique[oneLevel+'/'+secondLevel] = 1;
-
-				folders.push({
-					label: secondLevel.replace(/(^|\/)[0-9\. ]+/g, '$1'),
-					sortTitle: data.originalName,
-					link: '/' + oneLevel+'/'+secondLevel + '/index.html',
-					class: 'guide-nav-item level-2'
-				});
-			}
-
-			if (unique[oneLevel]) {
-				continue;
-			}
-			unique[oneLevel] = 1;
-
-			folders.push({
-				label: oneLevel.replace(/(^|\/)[0-9\. ]+/g, '$1'),
-				sortTitle: data.originalName,
-				link: '/' + oneLevel + '/index.html',
-				class: 'guide-nav-item'
-			});
-		}
-
-		// console.log('folders ->',folders);
-
-		// files.forEach(function(file) {
-		//   folders[file.substr(0,file.lastIndexOf('/'))] = 1;
-		// });
-
-		var newFolders = _.sortBy(folders, 'sortTitle');
-
-		// console.log('newFolders ->',newFolders);
-
-		// newFolders.forEach(function(folder, i, list) {
-		// 	list[i] = ;
-		// });
-
-		// console.log('newFolders->', newFolders);
-
-		for (var file in files) {
-			files[file].mainNav = newFolders;
-		}
-
-		done();
-	}
-};
 
 module.exports = generateMenu;
 
